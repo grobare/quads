00c069bf649c4224d142ea55ff1fcf825f6eb840
m.erdevicki <m.erdevicki@yahoo.com>
2022-04-10 15:42:23 +0300

    this is commit

--- quadA.go
+++ quadA.go
@@ -2,7 +2,7 @@ package piscine
 
 import "fmt"
 
-/*func CreateFirstAndLastRow(w int) {
+func CreateFirstAndLastRow(w int) {
 	//w is width of rectangle
 	for i := 0; i < w; i++ {
 		if i == 0 || i == w-1 {
@@ -12,7 +12,7 @@ import "fmt"
 		}
 	}
 	fmt.Print("\n")
-} */
+}
 
 func CreateRow(w int) {
 	//w is width of rectange as in characters used to do it
